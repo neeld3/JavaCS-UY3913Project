5e159bb19e928b9fa2f76593fbce8e3b425e4fe5
neel <neel@dahake.com>
2025-05-10 17:58:59 -0400

    comments

--- server.java
+++ server.java
@@ -8,9 +8,10 @@ public class Server {
     public static Database shared_database;  
     public static void main(String[] args) {
         try {
+            //initializes server
             ServerSocket server = new ServerSocket(5190);
             shared_database = new Database();
-            
+            //accepts clients
             while(true){
                 Socket socket = server.accept(); 
                 new ProcessConnection(socket, shared_database).start();
@@ -21,7 +22,7 @@ public class Server {
     }
 }
 
-
+//database class for clients
 class Database {
     public Connection conn;
 
@@ -31,10 +32,11 @@ class Database {
 
     // Handling loggin in
     public int login(String username, String password, PrintStream out) throws SQLException {
+        //gets the information about username and password
         PreparedStatement query = conn.prepareStatement("SELECT user_id, password FROM user WHERE username = ?");
         query.setString(1, username);
         ResultSet result = query.executeQuery();
-
+        //if the password matches logs in else error
         if (result.next()) {
             String stored_pass = result.getString("password");
           
@@ -54,6 +56,7 @@ class Database {
 
     // Handling registering new user
     public int signup(String username, String password, PrintStream out) throws SQLException {
+        //checks if username is unique
         username = username.toLowerCase();
         PreparedStatement check = conn.prepareStatement("SELECT * FROM User WHERE username = ?");
         check.setString(1, username);
@@ -62,6 +65,7 @@ class Database {
             out.println("Username already exists");
             return -1;
         }
+        //creates account
         PreparedStatement query = conn.prepareStatement("{CALL signUp(?, ?)}");
         String hashed_password = BCrypt.hashpw(password, BCrypt.gensalt());
         query.setString(1, username);
@@ -73,10 +77,12 @@ class Database {
 
     // Displaying all the accounts 
     public void displayAccounts(int session, PrintStream out) throws SQLException {
+        //gets accounts from database
         PreparedStatement query = conn.prepareStatement("{CALL get_accounts(?)}");
         query.setInt(1, session);
         ResultSet result = query.executeQuery();
 
+        //sends all accounts information to client that requested
         while (result.next()){
             int account_id = result.getInt("account_id");
             double balance = result.getDouble("balance");
@@ -90,6 +96,7 @@ class Database {
         out.println("END");
     }
 
+    //for when client asks for the username
     public void send_name(int session, PrintStream out) throws SQLException {
         PreparedStatement query = conn.prepareStatement("SELECT username FROM User WHERE user_id = ?");
         query.setInt(1, session);
@@ -101,12 +108,13 @@ class Database {
         }
     }
     
-     
+    //for when clients asks for all usernames
     public void view(int session, PrintStream out) throws SQLException {
         PreparedStatement query = conn.prepareStatement("{CALL view_transactions(?)}");
         query.setInt(1, session);
         ResultSet result = query.executeQuery();
 
+        //gets all transactions from query, formates and sends it to client.
         while (result.next()){
             String type = result.getString("type");
             int sender_acc_id = result.getInt("sender_account_id");
@@ -126,12 +134,14 @@ class Database {
         out.println("END");
     }
  
-
+    //when client deposits money
     public synchronized void deposit(int account_id, double amount, int session, PrintStream out) throws SQLException {
+        //checks for valid amount
         if (amount <= 0){
             out.println("Deposit amount has to be greater than $0");
             return;
         }
+        //updates database, checks matching account, and then updates values
         PreparedStatement check_user = conn.prepareStatement("SELECT * FROM user_accounts WHERE user_id = ? AND account_id = ?");
         check_user.setInt(1, session);
         check_user.setInt(2, account_id);
@@ -148,11 +158,14 @@ class Database {
         }
     }
 
+    //when client withdraws money
     public synchronized void withdraw(int account_id, double amount, int session, PrintStream out) throws SQLException {
+        //checks for valid amount
         if (amount <= 0){
             out.println("Withdraw amount has to be greater than $0");
             return;
         }
+        //updates database, checks matching account, and then updates values
         PreparedStatement check_user = conn.prepareStatement("SELECT * FROM user_accounts WHERE user_id = ? AND account_id = ?");
         check_user.setInt(1, session);
         check_user.setInt(2, account_id);
@@ -181,13 +194,16 @@ class Database {
             out.println("Withdraw failed.");
         }
     }
-    
+    //when client sends money to another client
     public synchronized void send(int sender_account_id, int receiver_id, int receiver_account_id, double amount, int session, PrintStream out) throws SQLException {
+        //ensuring a valid amount is sent
         if (amount <= 0){
             out.println("Send amount has to be greater than $0");
             return;
         }
         conn.setAutoCommit(false);
+        
+        //sends all updates to the database
         try {
             PreparedStatement check_sender = conn.prepareStatement("SELECT * FROM user_accounts WHERE user_id = ? AND account_id = ?");
             check_sender.setInt(1, session);
@@ -243,7 +259,7 @@ class Database {
         }
         
     }
-
+    //inserts valid transactions to data base
     public void record_transaction(int user1_id, int user1_account_id, Integer user2, Integer user2_account_id, double amount, String type) throws SQLException {
         if (user2 != null && user2_account_id != null) {
             PreparedStatement insert_info = conn.prepareStatement("INSERT INTO transaction (Type, sender_id, sender_account_id, receiver_id, receiver_account_id, amount, timestamp) VALUES (?, ?, ?, ?, ?, ?, NOW())");
@@ -283,39 +299,46 @@ class ProcessConnection extends Thread{
         try {
             Scanner sin = new Scanner(socket.getInputStream());
             PrintStream sout = new PrintStream(socket.getOutputStream()); 
-
+            //reads from the client
             while (sin.hasNextLine()) {
                 String input = sin.nextLine();
                 String[] user_input = input.split(" ");
-              
+                // if its login command from user
                 if (user_input[0].equals("LOGIN")) {
                     int user_id = database.login(user_input[1], user_input[2], sout);
                     if (user_id != -1) {
                         session = user_id;
                     }
+                //if its create account command from user
                 } else if (user_input[0].equals("CREATE")) {
                     int user_id = database.signup(user_input[1], user_input[2], sout);
                     if (user_id != -1) {
                         session = user_id;
                     }
+                //if user asks for accounts
                 } else if (user_input[0].equals("DISPLAY_ACCOUNTS")) {
                     database.displayAccounts(session, sout);
+                //if user is depositing
                 } else if (user_input[0].equals("DEPOSIT")) {
                     int account_id = Integer.parseInt(user_input[1]);
                     double amount = Double.parseDouble(user_input[2]);
                     database.deposit(account_id, amount, session, sout);
+                //if user is withdrawing
                 } else if (user_input[0].equals("WITHDRAW")) {
                     int account_id = Integer.parseInt(user_input[1]);
                     double amount = Double.parseDouble(user_input[2]);
                     database.withdraw(account_id, amount, session, sout);
+                //if user is sending to another 
                 } else if (user_input[0].equals("SEND")) {
                     int sender_account_id = Integer.parseInt(user_input[1]);
                     int receiver_id = Integer.parseInt(user_input[2]);
                     int receiver_account_id = Integer.parseInt(user_input[3]);
                     double amount = Double.parseDouble(user_input[4]);
                     database.send(sender_account_id, receiver_id, receiver_account_id, amount, session, sout);
+                //if user wants to view transactions
                 } else if (user_input[0].equals("VIEW")) {
                     database.view(session, sout);
+                //gets the users name
                 } else if (user_input[0].equals("NAME")) {
                     database.send_name(session, sout);
                 }
